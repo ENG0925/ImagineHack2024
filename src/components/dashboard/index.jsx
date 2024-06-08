250f326e0f047303ba2b3861172b222d3dd74348
LTW03 <108678641+LTW03@users.noreply.github.com>
2024-06-09 05:24:08 +0800

    dashboard containers uodate

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -24,14 +24,14 @@ const Charts = () => {
       </section>
 
       <section className="flex my-4 px-4 gap-3">
-        <div className="w-1/2 h-[300px] bg-gray-700 rounded"></div>
-        <div className="w-1/2 h-[300px] bg-gray-700 rounded"></div>
+        <div className="w-full sm:w-1/2 h-[300px] bg-gray-700 rounded"></div>
+        <div className="w-full sm:w-1/2 h-[300px] bg-gray-700 rounded"></div>
       </section>
 
       <section className="flex my-4 px-4 gap-2">
-        <div className=" w-1/3 h-[250px] bg-gray-700 rounded"></div>
-        <div className=" w-1/3 h-[250px] bg-gray-700 rounded"></div>
-        <div className=" w-1/3 h-[250px] bg-gray-700 rounded"></div>
+        <div className="w-full sm:w-1/3 h-[250px] bg-gray-700 rounded"></div>
+        <div className="w-full sm:w-1/3 h-[250px] bg-gray-700 rounded"></div>
+        <div className="w-full sm:w-1/3 h-[250px] bg-gray-700 rounded"></div>
       </section>
     </>
   );
