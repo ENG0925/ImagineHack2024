7699ccae162f504d9620dc127b2d5a57e6f57ea7
WeiJiun1234 <weijiun.kidydult@one2deal.xyz>
2024-06-08 19:43:52 +0800

    pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,6 @@
 import { OCRscan } from "@/lib/function"; // Import the OCRscan function
 
 export default function Home() {
-  
-  const handleOCR = async () => {
-    try {
-      const response = await OCRscan('invoice.jpg');
-      console.log(response); // Log the response from the OCR function
-    } catch (error) {
-      console.error("Error during OCR scan:", error); // Handle and log any errors
-    }
-  };
 
   return (
     <div>
